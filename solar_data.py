c25c8b0ce2495df6c6a379f2f89a9dad854ed250
Ali Mustafa <Mohammadali7477@gmail.com>
2025-02-26 16:25:09 -0500

    Update solar_data.py

--- solar_data.py
+++ solar_data.py
@@ -1,34 +1,35 @@
 import pandas as pd
 import sqlite3 
 
-#Connect to DB
-conn=sqlite3.connect("./databases/db.sql")
-cursor=conn.cursor()
+# Connect to DB
+conn = sqlite3.connect("./databases/db.sql")
+cursor = conn.cursor()
 
-#Load Datasets 
-energy_df=pd.read_csv('./data/energy.csv')
-weather_df=pd.read_csv('./data/weather.csv')
+# Load Datasets 
+energy_df = pd.read_csv('./data/energy.csv')
+weather_df = pd.read_csv('./data/weather.csv')
 
 # See the first 5 rows 
 print("Energy_Sample:")
 example = energy_df.head()
+print(example)
 
-#Statistics for the energy
-energy_mean=energy_df.mean()
-energy_median=energy_df.median()
-energy_std=energy_df.std()
+# Statistics for the energy
+energy_mean = energy_df.mean()
+energy_median = energy_df.median()
+energy_std = energy_df.std()
 
 # Statistics for the weather
-weather_std=weather_.std()
-weather_mean=weather_df.mean()
-weather_median=energy_df.median()
+weather_mean = weather_df.mean()
+weather_median = weather_df.median()
+weather_std = weather_df.std()
 
-#Store results into a Dataframe 
-stats_df = pd.DataFrame ({
-    'Energy_Mean'=energy_mean,
-    'Energy_Median'=energy_median,
-    'Energy_STD'=energy_std,
-    'Weather_Mean'=weather_mean,
-    'Weather_Median'=weather_median,
-    'Weather_STD'=weather_std
+# Store results into a DataFrame 
+stats_df = pd.DataFrame({
+    'Energy_Mean': energy_mean,
+    'Energy_Median': energy_median,
+    'Energy_STD': energy_std,
+    'Weather_Mean': weather_mean,
+    'Weather_Median': weather_median,
+    'Weather_STD': weather_std
 })
